bea040ab080f9a09ef8e087947edc86c8bfb0a68
hr <spc10p@gmail.com>
2020-08-03 11:19:45 +0000

    post

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,4 +55,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-end
+
+  config.web_console.whitelisted_ips = '133.218.8.1'
+  
+end  
+  
+  
+  
