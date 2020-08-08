b2baff83bf238fc5759561bd646bb8599b2cc74a
hr <spc10p@gmail.com>
2020-08-08 03:05:42 +0000

    kadai1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only:[:destroy]
+  before_action :set_task, only:[:show, :edit, :update,:destroy]
 
   def index
     if logged_in?
@@ -9,28 +10,52 @@ class TasksController < ApplicationController
     end
   end
   
+  def show
+  end
+  
+  def new
+    @task = Task.new
+  end 
+  
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'メッセージを投稿しました。'
+      flash[:success] = 'Taskを投稿しました。'
       redirect_to root_url
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
-      render 'toppages/index'
+      flash.now[:danger] = 'Taskの投稿に失敗しました。'
+      redirect_to root_url
     end
   end
+  
+  def edit
+  end
 
+  def update
+    if @task.update(task_params)
+      flash[:task] = 'Taskは正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Taskは更新されませんでした'
+      render edit
+    end
+  end
+  
   def destroy
     @task.destroy
-    flash[:success] = 'メッセージを削除しました。'
-    redirect_back(fallback_location: root_path)
+    flash[:success] = 'Taskを削除しました。'
+    redirect_to root_url
   end
   
   private
   
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content,:status)
   end
   
   def correct_user
