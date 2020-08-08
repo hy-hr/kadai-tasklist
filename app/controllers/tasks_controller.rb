d0d2216748563341078dcb881ca9e7cfe23bd298
hr <spc10p@gmail.com>
2020-08-08 09:22:00 +0000

    kadai1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,10 +4,8 @@ class TasksController < ApplicationController
  
 
   def index
-    if logged_in?
-      @task = current_user.tasks.build
+      
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
   end
   
   def show
